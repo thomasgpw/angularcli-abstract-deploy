f20695fa66120dc702c354d9197b475e0ce8ccd9
Thomas Greenstein <thomasgpw@gmail.com>
2018-02-16 14:10:59 -0500

    working to make angular cli project build on heroku 2

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js as according to https://medium.com/@ryanchenkie_40935/angular-cli-deployment-host-your-angular-2-app-on-heroku-3f266f13f352c
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
